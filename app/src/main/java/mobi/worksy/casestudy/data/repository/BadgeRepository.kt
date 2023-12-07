269f9fa89a5c349c5a1ace4968cde1388196bf03
oguncan <oguncanlnx@gmail.com>
2023-12-07 17:31:13 +0300

    -Adding repository functions

--- app/src/main/java/mobi/worksy/casestudy/data/repository/BadgeRepository.kt
+++ app/src/main/java/mobi/worksy/casestudy/data/repository/BadgeRepository.kt
@@ -1,9 +1,21 @@
 package mobi.worksy.casestudy.data.repository
 
+import mobi.worksy.casestudy.data.model.BadgeModel
 import mobi.worksy.casestudy.data.remote.BadgeApi
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.Response
 import javax.inject.Inject
 
 class BadgeRepository @Inject constructor(
-    val badgeApi: BadgeApi
+    private val badgeApi: BadgeApi
 ) {
+
+    suspend fun getBadgeList() : Response<BadgeModel> {
+        val requestBody =
+            "{\"endpoint\": \"badgeList\"}".toRequestBody("application/json".toMediaTypeOrNull())
+        return badgeApi.getBadgeList(requestBody)
+    }
 }
--- app/src/main/java/mobi/worksy/casestudy/data/repository/PraiseRepository.kt
+++ app/src/main/java/mobi/worksy/casestudy/data/repository/PraiseRepository.kt
@@ -1,9 +1,19 @@
 package mobi.worksy.casestudy.data.repository
 
+import mobi.worksy.casestudy.data.model.BadgeModel
+import mobi.worksy.casestudy.data.model.PraiseModel
 import mobi.worksy.casestudy.data.remote.PraiseApi
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.Response
 import javax.inject.Inject
 
 class PraiseRepository @Inject constructor(
-    val praiseApi: PraiseApi
+    private val praiseApi: PraiseApi
 ) {
+    suspend fun getBadgeList() : Response<PraiseModel> {
+        val requestBody =
+            "{\"endpoint\": \"praiseList\"}".toRequestBody("application/json".toMediaTypeOrNull())
+        return praiseApi.getPraiseList(requestBody)
+    }
 }
